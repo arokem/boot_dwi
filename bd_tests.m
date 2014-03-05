bbb15c56d139e4e46fc0a66c9179daa321efd3b2
arokem <arokem@gmail.com>
2014-03-05 09:02:21 -0800

    BF + RF: Modularize further. Make sure that output points are unique.

--- bd_tests.m
+++ bd_tests.m
@@ -1,52 +1,59 @@
-% Provides a test-suite for the functions used here: 
+% Provides a test-suite for the functions used here:
 
 % vector_angle (also tests other functions, such as unit_vector, l2_norm,
-% etc). 
+% etc).
 
-% Orthogonal vectors: 
-a = [1 0 0]; 
-b = [0 1 0]; 
+% Orthogonal vectors:
+a = [1 0 0];
+b = [0 1 0];
 
-assertEqual(vector_angle(a,b), pi/2); 
+assertEqual(vector_angle(a,b), pi/2);
 
-% Equal vectors: 
-a = [randn, randn, randn]; 
-b = a; 
-% Should be an angle of 0: 
-assertEqual(vector_angle(a,b), 0); 
+% Equal vectors:
+a = [randn, randn, randn];
+b = a;
+% Should be an angle of 0:
+assertEqual(vector_angle(a,b), 0);
 
-% Same direction: 
-a = [1 0 0]; 
-b = [0.5 0 0]; 
+% Same direction:
+a = [1 0 0];
+b = [0.5 0 0];
 assertEqual(vector_angle(a, b), 0);
 
-% Opposite direction: 
+% Opposite direction:
 
-a = [1 0 0]; 
-b = [-0.5 0 0]; 
+a = [1 0 0];
+b = [-0.5 0 0];
 
-assertEqual(vector_angle(a,b), pi); 
+assertEqual(vector_angle(a,b), pi);
 
-% a = unit_vector([randn randn randn]); 
-% b = unit_vector([randn randn randn]); 
+% a = unit_vector([randn randn randn]);
+% b = unit_vector([randn randn randn]);
 
-a = [1 0 0]; 
-b = [1 0 0]; 
-rot = calculate_rotation(a,b); 
+a = [1 0 0];
+b = [1 0 0];
+rot = calculate_rotation(a,b);
 
 assertEqual(a', rot *b')
 
-a = [1 0 0]; 
-b = [0 1 0]; 
-rot = calculate_rotation(a,b); 
+a = [1 0 0];
+b = [0 1 0];
+rot = calculate_rotation(a,b);
 
 assertAlmostEqual(a', rot *b')
 
-a = [1 0 0]; 
-b = [0 0 1]; 
-rot = calculate_rotation(a,b); 
+a = [1 0 0];
+b = [0 0 1];
+rot = calculate_rotation(a,b);
 
 assertAlmostEqual(a', rot *b')
 
+
+% Test that the bvectors you get back are unique:
+for i=1:10
+    bvecs_orig = bd_read_epoints(100);
+    [xyz, idx] = bd_subsample(bv_orig', 30);
+    assertEqual(length(unique(idx)), length(idx));
+end
 
 
